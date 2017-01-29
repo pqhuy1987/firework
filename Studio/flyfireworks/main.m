8460586e6aea7aee93a48b8a67e31c0fabbf23b1
hvtung <fastman0170@gmail.com>
2017-01-29 23:02:19 +0700

    Init

---
+++ Studio/flyfireworks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  kuang
+//
+//  Created by garyliumac on 14-1-14.
+//  Copyright zlot 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
